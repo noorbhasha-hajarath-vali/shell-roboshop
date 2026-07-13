3341e1d075ef524a3f4f10e01b7cd7fd137dbd3b
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-13 19:10:49 +0530

    Testing Create Instance

--- roboshop.sh
+++ roboshop.sh
@@ -3,16 +3,9 @@
 AMI_ID=ami-002192a70217ac181
 SG_ID=sg-014ee579326daf5b9
 
-
 for INSTANCE in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --key-name devops --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" --query 'Instances[0].InstanceId' --output text)
 
-    if [ $INSTANCE != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text) # get private instance id
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text) # get public instance id
-    fi
-
-    echo $INSTANCE: $IP
+    echo "$INSTANCE_ID"
 done
