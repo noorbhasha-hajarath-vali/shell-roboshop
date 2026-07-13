1baaef846401aa9d6e56546d56fc1734a14487c3
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-13 23:52:52 +0530

    commiting roboshop file

--- roboshop.sh
+++ roboshop.sh
@@ -3,15 +3,18 @@
 AMI_ID=ami-002192a70217ac181
 SG_ID=sg-014ee579326daf5b9
 ZONE_ID=Z09974681JREIYBUXFTO2
+DOMAIN="ayri.fun"
 
 for INSTANCE in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --key-name devops --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" --query 'Instances[0].InstanceId' --output text)
 
-    if [ INSTANCE != "frontend" ]; then
+    if [ "INSTANCE" != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$INSTANCE.$DOMAIN"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN"
     fi
 
     aws route53 change-resource-record-sets \
@@ -20,7 +23,7 @@ do
     \"Changes\": [{
       \"Action\": \"UPSERT\",
       \"ResourceRecordSet\": {
-        \"Name\": \"$INSTANCE.ayri.fun\",
+        \"Name\": \"$RECORD_NAME\",
         \"Type\": \"A\",
         \"TTL\": 1,
         \"ResourceRecords\": [{
