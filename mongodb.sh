127a4f7be3ca28c134d2f190c9c2dada8ee53d02
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-15 23:16:39 +0530

    Practice

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ LOG_DIR=/var/log/shell-roboshop
 FILE_NAME=$(basename $0 .sh)
 LOG_FILE=$LOG_DIR/$FILE_NAME.log
 
-if [$USER_ID -ne 0 ]; then
+if [ $USER_ID -ne 0 ]; then
     echo "User doesn't have root privilages, Run with sudo"
     exit 1
 fi
