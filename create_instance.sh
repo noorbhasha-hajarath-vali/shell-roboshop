aad9d67289ca2407041216009f26f741c5d4d2b4
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-15 23:35:24 +0530

    Practice

--- create_instance.sh
+++ create_instance.sh
@@ -13,6 +13,18 @@ do
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
     --query 'Instances[0].InstanceId' \
     --output text)
+
+    if [ $INSTANCE != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
+    else
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
+    fi
 done
 
-echo "$INSTANCE: $INSTANCE_ID"
+echo "$INSTANCE: $IP"
