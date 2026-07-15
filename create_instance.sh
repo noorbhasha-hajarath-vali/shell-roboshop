eb8cfceca68b00d25f82f1f46e1b9dbff9d1313a
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-16 00:10:51 +0530

    Practice

--- create_instance.sh
+++ create_instance.sh
@@ -2,6 +2,7 @@
 
 AMI_ID=ami-002192a70217ac181
 SG_ID=sg-014ee579326daf5b9
+DOMAIN="ayri.fun"
 
 for INSTANCE in $@
 do
@@ -19,12 +20,47 @@ do
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text)
+            RECORD_NAME="$INSTANCE.$DOMAIN"
     else
         IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text)
+            RECORD_NAME="$DOMAIN"
     fi
 
-    echo "$INSTANCE: $IP"
+    ZONE_ID=$(aws route53 list-hosted-zones-by-name \
+    --dns-name "$DOMAIN" \
+    --query "HostedZones[?Name=='$DOMAIN.'].Id | [0]" \
+    --output text)
+
+    if [ "$ZONE_ID" = "None" ]; then
+        ZONE_ID=$(aws route53 create-hosted-zone \
+            --name "$DOMAIN" \
+            --caller-reference "$(date +%s)" \
+            --query 'HostedZone.Id' \
+            --output text)
+
+        # Remove "/hostedzone/" from the returned value
+        ZONE_ID=${ZONE_ID##*/}
+    else
+        # Remove "/hostedzone/" from the returned value
+        ZONE_ID=${ZONE_ID##*/}
+    fi
+
+    # aws route53 change-resource-record-sets \
+    # --hosted-zone-id "$ZONE_ID" \
+    # --change-batch '{
+    #     "Changes": [{
+    #     "Action": "UPSERT",
+    #     "ResourceRecordSet": {
+    #         "Name": "'"$RECORD_NAME"'",
+    #         "Type": "A",
+    #         "TTL": 1,
+    #         "ResourceRecords": [{
+    #         "Value": "'"$IP"'"
+    #         }]
+    #     }
+    #     }]
+    # }'
 done
