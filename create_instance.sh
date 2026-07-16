aaf85ca7269766cf06d74d8a5e3e40b1e589e00c
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-16 07:12:07 +0530

    Practice

--- create_instance.sh
+++ create_instance.sh
@@ -4,4 +4,9 @@ AMI_ID=ami-002192a70217ac181
 SG_ID=sg-014ee579326daf5b9
 DOMAIN="ayri.fun"
 
-aws route53 list-hosted-zones
+ZONE_ID=$(aws route53 list-hosted-zones-by-name \
+    --dns-name "$DOMAIN" \
+    --query "HostedZones[?Name=='$DOMAIN.'].Id | [0]" \
+    --output text)
+
+echo "$ZONE_ID"
